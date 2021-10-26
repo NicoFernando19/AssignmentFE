9b9a109e18d3821d4a40051dd0f09f7bac5a2d17
NicoFernando19 <nicofernando39@gmail.com>
2021-10-27 00:05:42 +0700

    added login, register, forgot password, and reset password pages and connect with api

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  )
 }
 
 export default MyApp
